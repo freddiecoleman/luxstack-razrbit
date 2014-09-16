14385e88a77a264dc30d9e1c8f1dfe05104a9c57
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-09-16 22:06:15 +0100

    initial phpspec test

--- spec/Luxstack/Razrbit/RazrbitSpec.php
+++ spec/Luxstack/Razrbit/RazrbitSpec.php
@@ -7,6 +7,11 @@ use Prophecy\Argument;
 
 class RazrbitSpec extends ObjectBehavior
 {
+    function let($appId = 'A25AOpLUoT',$appSecret = '688e2b77-09a3-4945-9468-bf188ff3de88')
+    {
+        $this->beConstructedWith($appId, $appSecret);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Luxstack\Razrbit\Razrbit');
